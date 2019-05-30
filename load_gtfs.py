316f8a86533e04b81788c09c63203d7e84bec3e5
Dakad <3106338+Dakad@users.noreply.github.com>
2019-05-30 13:12:26 +0200

    Set-up the process alo to run for the load

--- load_gtfs.py
+++ load_gtfs.py
@@ -6,8 +6,8 @@ from config import Config
 
 
 def _read_gtfs_feed(feed_file_name):
-    with open(feed_file_name, "r") as gtfs:
-        gtfs_reader = csv.reader(gtfs)
+    with open(feed_file_name, "r") as gtfs_file:
+        gtfs_reader = csv.reader(gtfs_file)
         head = next(gtfs_reader)
 
         for row in gtfs_reader:
@@ -18,6 +18,17 @@ def _read_gtfs_feed(feed_file_name):
 
 
 def run():
+
+    # TODO Get the DB_URI from Config
+
+    # TODO Init the db and get a DbSession
+
+    # TODO Loop on the gtfs
+        # TODO Read the gtfs file
+        # TODO Insert each line
+        # TODO Bulk add_all into the session
+        # TODO Commit on each 50 % read
+
     pass
 
 
