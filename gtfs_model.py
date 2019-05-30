4c2deed4fdc8376c22d9c44842647f81ce90d795
Dakad <3106338+Dakad@users.noreply.github.com>
2019-05-30 12:17:09 +0200

    Add primary  and index keys to gtfs models

--- gtfs_model.py
+++ gtfs_model.py
@@ -12,7 +12,8 @@ class Agency(Base):
     __tablename__ = "agency"
     __plural_name__ = "agencies"
 
-    agency_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    agency_id = Column(Integer, index=True)
     agency_name = Column(Unicode(255))
     agency_url = Column(Unicode(255))
     agency_timezone = Column(String(50))
@@ -23,7 +24,8 @@ class Route(Base):
     __tablename__ = "route"
     __plural_name__ = "routes"
 
-    route_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    route_id = Column(Integer, index=True)
     agency_id = Column(Integer)
     route_short_name = Column(Unicode(150), index=True)
     route_long_name = Column(Unicode(255), index=True)
@@ -35,7 +37,8 @@ class StopTime(Base):
     __tablename__ = "stop_time"
     __plural_name__ = "stop_times"
 
-    trip_id = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    trip_id = Column(Integer, index=True)
     arrival_time = Column(String(8))
     departure_time = Column(String(8))
     stop_id = Column(Integer)
@@ -48,7 +51,8 @@ class Stop(Base):
     __tablename__ = "stop"
     __plural_name__ = "stops"
 
-    stop_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    stop_id = Column(Integer, index=True)
     stop_name = Column(Unicode(255), index=True)
     stop_desc = Column(Unicode(255))
     stop_lat = Column(String(50))
@@ -60,9 +64,10 @@ class Trip(Base):
     __tablename__ = "trip"
     __plural_name__ = "trips"
 
+    id = Column(Integer, primary_key=True)
+    trip_id = Column(Integer, index=True)
     route_id = Column(Integer)
     service_id = Column(Integer)
-    trip_id = Column(Integer, primary_key=True)
     trip_headsign = Column(String(255), index=True)
     direction_id = Column(Integer)
     block_id = Column(Integer)
