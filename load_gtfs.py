d53b53ee7827fcda67cda3d2b87477d46ef3dbba
Dakad <3106338+Dakad@users.noreply.github.com>
2019-05-31 01:58:25 +0200

    Use bulk insert with enumerate index instead of counter

--- load_gtfs.py
+++ load_gtfs.py
@@ -5,6 +5,8 @@ import os
 
 from config import Config
 from gtfs_model import init as db_init, get_class_by_gtfs_filename
+from gtfs_model import get_class_by_gtfs_filename
+from gtfs_model import list_gtfs_model_tablenames
 
 
 def _read_gtfs_feed(feed_file_name):
@@ -21,22 +23,31 @@ def _read_gtfs_feed(feed_file_name):
 
 def run():
 
-    # TODO Get the DB_URI from Config
     DbSession = db_init(Config.DB_URI, Config.DEBUG)
+
+    model_tablenames = list_gtfs_model_tablenames()
+    print(model_tablenames)
 
     for gtfs_filename in os.listdir(Config.GTFS_DIR):
         filename, ext = os.path.splitext(gtfs_filename)
         if 'txt' not in ext:
             continue
+
         GTFSModel = get_class_by_gtfs_filename(filename)
+        if(GTFSModel == None):
+            continue
 
         feed_filename = os.path.join(Config.GTFS_DIR, gtfs_filename)
-        read_counter = 0
-        for feed in _read_gtfs_feed(feed_filename):
+
+        for counter, feed in enumerate(_read_gtfs_feed(feed_filename)):
             gtfs_feed = GTFSModel(**feed)
             DbSession.add(gtfs_feed)
 
-        DbSession.commit()
+            if counter % 5 == 0:
+                DbSession.commit()
+
+        if DbSession.is_active:
+            DbSession.commit()
 
     # TODO Insert each line
     # TODO Bulk add_all into the session
